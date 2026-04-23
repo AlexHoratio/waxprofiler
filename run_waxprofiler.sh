1299c89c38d5a9c11e5cfcc65be89eab7338ce3b
yukithetupper <horatioesf@protonmail.com>
2026-04-23 12:51:03 +0100

    fixed run script typo

--- run_waxprofiler.sh
+++ run_waxprofiler.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nextflow run main.nf -profile singularity --input /home/alex/Desktop/waxprofiler_data/samplesheet.csv --databases /home/alex/Desktop/waxprofiler_data/databases.csv --output /home/alex/Desktop/waxprofiler_output
+nextflow run main.nf -profile singularity --input /home/alex/Desktop/waxprofiler_data/samplesheet.csv --databases /home/alex/Desktop/waxprofiler_data/databases.csv --outdir /home/alex/Desktop/waxprofiler_output

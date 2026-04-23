399e7baf769d8459a9f4c81d0c241ace4a0b5816
yukithetupper <horatioesf@protonmail.com>
2026-04-23 12:50:13 +0100

    run script for ez

---
+++ run_waxprofiler.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nextflow run main.nf -profile singularity --input /home/alex/Desktop/waxprofiler_data/samplesheet.csv --databases /home/alex/Desktop/waxprofiler_data/databases.csv --output /home/alex/Desktop/waxprofiler_output
